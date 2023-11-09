c6c88f790de83b8531ddadb96de0f42d0829f532
ju <Seregina.j.s@gmail.com>
2023-11-09 19:56:53 +0200

    4

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, url_for, session, flash, req
 import database
 from models import User, Customer, Item, ItemGroup, Order, OrderItem, Stock, SellingPrice, CostPrice, OrderStatus
 from flask_session import Session
+from sqlalchemy import desc
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
@@ -29,19 +30,22 @@ def login():
         user_name = request.form.get('name')
         user_password = request.form.get('password')
         find_user = database.db_session.query(User).filter_by(name=user_name, password=user_password).first()
-
         if not find_user:
-            flash('Unknown username')
             return redirect(url_for('login'))
         else:
             session['user_id'] = find_user.id
             session['name'] = find_user.name
-            return redirect(url_for('user_database', user=find_user.name))
+            return redirect(url_for('user_main_page', user=find_user.name))
     return render_template('login.html')
 
 
+@app.route('/logout/')
+def logout():
+    session.pop('name')
+    return redirect(url_for('login'))
+
 @app.route('/crm/', methods=['GET', 'POST'])
-def user_database():
+def user_main_page():
     return render_template('crm.html')
 
 
@@ -50,9 +54,19 @@ def user_database():
 
 @app.route('/item_groups/', methods=['GET', 'POST'])
 def item_group_add():
-    return render_template('index.html')
+    database.init_db()
+    user_name = session.get('name', None)
+    current_user = session.get('user_id', None)
+    if request.method == 'POST':
+        name = request.form.get('name')
+        item_group_new = ItemGroup(name, current_user)
+        database.db_session.add(item_group_new)
+        database.db_session.commit()
+    item_groups_all = database.db_session.query(ItemGroup).\
+        filter_by(user_id=current_user).order_by(ItemGroup.name).all()
+    return render_template('item_groups_add.html', item_groups=item_groups_all, user=user_name)
 
-# Create new and View products
+
 @app.route('/items/', methods=['GET', 'POST'])
 def item_add():
     return render_template('index.html')
@@ -60,43 +74,76 @@ def item_add():
 
 # Edit product
 @app.route('/items/<int:item_id>/', methods=['GET', 'POST'])
-def item_edit():
+def item_edit(item_id):
     return render_template('index.html')
 
 
 # Add to product new price
 @app.route('/items/<int:item_id>/price', methods=['POST'])
-def item_price_add():
+def item_price_add(item_id):
     return render_template('index.html')
 
 # Increase product stock
-@app.route('/items/<int:items_id>/stock_add/', methods=['POST'])
-def item_stock_add():
+@app.route('/items/<int:item_id>/stock_add/', methods=['POST'])
+def item_stock_add(item_id):
     return render_template('index.html')
 
 
 # Decrease product stock
 @app.route('/items/<int:item_id>/stock_decrease/', methods=['POST'])
-def item_stock_decrease():
+def item_stock_decrease(item_id):
     return render_template('index.html')
 
-#Create a new customer and view customer
-@app.route('/customers/', methods=['GET, POST'])
+
+@app.route('/customers/', methods=['GET'])
+def customers():
+    database.init_db()
+    user_name = session.get('name', None)
+    current_user = session.get('user_id', None)
+    customers_all = database.db_session.query(Customer).\
+        filter_by(user_id=current_user).order_by(Customer.name).all()
+    return render_template('customers.html', customers_all=customers_all, user=user_name)
+
+
+@app.route('/customers/add/', methods=['POST', 'GET'])
 def customer_add():
-    return render_template('index.html')
+    database.init_db()
+    current_user = session.get('user_id', None)
+    user_name = session.get('name', None)
+    if request.method == 'POST':
+        customer_name = request.form.get('name')
+        customer_phone = request.form.get('phone')
+        customer_mail = request.form.get('mail')
+        customer_add_new = Customer(customer_name, current_user, customer_phone, customer_mail)
+        database.db_session.add(customer_add_new)
+        database.db_session.commit()
+        return redirect(url_for('customers'))
+    return render_template('customer_add.html', user=user_name)
 
-# Create a new order status and view
-@app.route('/order_statuses/',methods=['GET, POST'])
+@app.route('/customers/<int:customer_id>/', methods=['POST', 'GET'])
+def customer_delete(customer_id):
+    database.init_db()
+    current_user = session.get('user_id', None)
+    user_name = session.get('name', None)
+    if request.method == 'POST':
+        pass
+    customer_info = database.db_session.query(Customer).filter_by(id=customer_id).first()
+    return render_template('customer_delete.html', customer=customer_info, customer_id=customer_id, user=user_name)
+
+#Don't work!!!
+
+
+@app.route('/order_statuses/', methods=['GET', 'POST'])
 def order_statuses_add():
     return render_template('index.html')
 
 
-@app.route('/orders/', methods=['GET, POST'])
+@app.route('/orders/', methods=['GET', 'POST'])
 def order_add():
     return render_template('index.html')
 
-@app.route('/orders/<int:order_id>/', methods=['GET, POST'])
-def order_status_edit():
+@app.route('/orders/<int:order_id>/', methods=['GET', 'POST'])
+def order_status_edit(order_id):
     return render_template('index.html')
 
 @app.route('/report_sales_by_customers/', methods=['GET', 'POST'])
